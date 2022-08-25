77e6e97830282118fc5f8cb4ba91ead9ccbcf0ee
Marcos Morais <marcostmorais@gmail.com>
2022-08-25 19:51:01 -0300

    feat: SwiftyEmail functionality and styling are ready.

--- Sources/SwiftyEmail/SwiftyEmail.swift
+++ Sources/SwiftyEmail/SwiftyEmail.swift
@@ -1,6 +1,57 @@
-public struct SwiftyEmail {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import MessageUI
+import SwiftUI
 
-    public init() {
+open class SwiftyEmail: NSObject {
+    public static let shared = SwiftyEmail()
+
+    public func sendEmail(subject: String, body: String, to: String, completion: @escaping (Bool) -> Void){
+        if MFMailComposeViewController.canSendMail() {
+            let picker = MFMailComposeViewController()
+            picker.setSubject(subject)
+            picker.setMessageBody(body, isHTML: true)
+            picker.setToRecipients([to])
+            picker.mailComposeDelegate = self
+            
+            guard #available(iOS 11.0, *), let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            guard let window = scene.windows.first else { return }
+            
+            window.rootViewController?.present(picker, animated: true)
+        } else if let emailUrl = createEmailUrl(to: to, subject: subject, body: body) {
+            UIApplication.shared.open(emailUrl)
+        }
+        completion(MFMailComposeViewController.canSendMail())
+    }
+    
+    private func createEmailUrl(to: String, subject: String, body: String) -> URL? {
+        guard let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let bodyEncoded = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return nil
+        }
+
+        let gmailUrl = URL(string: "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)")
+        let yahooMail = URL(string: "ymail://mail/compose?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let defaultUrl = URL(string: "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        
+        if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
+            return gmailUrl
+        } else if let outlookUrl = outlookUrl, UIApplication.shared.canOpenURL(outlookUrl) {
+            return outlookUrl
+        } else if let yahooMail = yahooMail, UIApplication.shared.canOpenURL(yahooMail) {
+            return yahooMail
+        } else if let sparkUrl = sparkUrl, UIApplication.shared.canOpenURL(sparkUrl) {
+            return sparkUrl
+        }
+        
+        return defaultUrl
+    }
+
+}
+
+// MARK: - Protocol Conformance for MessageUI
+extension SwiftyEmail: MFMailComposeViewControllerDelegate {
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
