13515579a15460853bef2b825909bc393fe828fe
Marcos Morais <marcostmorais@gmail.com>
2022-08-25 20:42:46 -0300

    feat: New error was addad to EmailError.

--- Sources/SwiftyEmail/EmailError.swift
+++ Sources/SwiftyEmail/EmailError.swift
@@ -10,6 +10,7 @@ import Foundation
 public enum EmailError: LocalizedError {
     case unavailableApps
     case unknownError
+    case unableToPresentViewController
 
     public var errorDescription: String? {
         switch self {
@@ -17,6 +18,8 @@ public enum EmailError: LocalizedError {
             return NSLocalizedString("The user does not have any e-mail app available.", comment: "")
         case .unknownError:
             return NSLocalizedString("An unknown error occurred.", comment: "")
+        case .unableToPresentViewController:
+            return NSLocalizedString("The app wasn't able to present the mail view controller.", comment: "")
         }
     }
 }
