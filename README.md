8fd59e767cb4f3521f617d3dc8aa2ba1bd932e7d
Marcos Morais <marcostmorais@gmail.com>
2022-08-25 19:35:04 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyEmail
+
+A description of this package.
---
+++ Tests/SwiftyEmailTests/SwiftyEmailTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyEmail
+
+final class SwiftyEmailTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyEmail().text, "Hello, World!")
+    }
+}
